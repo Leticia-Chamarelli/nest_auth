3346074388ea1fbe1b9b582c298a1f91e3d9d991
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-05-08 18:08:15 -0300

    sign up method ready at app.service.ts

---
+++ src/app.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+const users = [];
+
+@Injectable()
+export class AppService {
+  async signUp(email: string, password: string) {
+    const existingUser  = users.find(user => user.email === email);
+    if (existingUser) {
+      return new BadRequestException('Email in use');
+    }
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = await scrypt(password, salt, 32) as Buffer;
+    const saltAndHash = `${salt}.${hash.toString('hex')}`;
+
+    const user = {
+      email,
+      password: saltAndHash,
+    };
+
+    users.push(user);
+
+    console.log('Signed up', user);
+    const { password: _,...result } = user;
+    return result;
+  }
+}
