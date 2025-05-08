0118ee1e8dd9203332df8ffaba721f95c1022e5a
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-05-08 18:53:21 -0300

    signin method done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,4 +10,10 @@ export class AuthController {
         const { email, password } = body;
         return this.authService.signUp(email, password);
     }
+
+    @Post('signin')
+    signIn(@Body() body: any) {
+        const { email, password } = body;
+        return this.authService.signIn(email, password);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
 
@@ -26,6 +26,24 @@ export class AuthService {
     users.push(user);
 
     console.log('Signed up', user);
+    const { password: _, ...result } = user;
+    return result;
+  }
+
+  async signIn(email: string, password: string) {
+    const user = users.find((user) => user.email === email);
+    if(!user) {
+        return new UnauthorizedException('Invalid credentials');
+    }
+    
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash != hash.toString('hex')) {
+        return new UnauthorizedException('Invalid credentials');
+    }
+
+    console.log('Signed in', user);
     const { password: _, ...result } = user;
     return result;
   }
