b1f4b733669e86535b4a1a3f7db536ada5c0e56e
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-05-08 18:25:04 -0300

    implement user signup with password hashing and salt using scrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+const users: { email: string; password: string }[] = [];
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async signUp(email: string, password: string) {
+    const existingUser = users.find(user => user.email === email);
+    if (existingUser) {
+      throw new BadRequestException('Email in use'); // use throw aqui!
+    }
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = await scrypt(password, salt, 32) as Buffer;
+    const saltAndHash = `${salt}.${hash.toString('hex')}`;
+
+    const user = {
+      email,
+      password: saltAndHash,
+    };
+
+    users.push(user);
+
+    console.log('Signed up', user);
+    const { password: _, ...result } = user;
+    return result;
+  }
+}
