ceb93b7aca7a50cc216a6b0f837383ce90d4ebef
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-05-08 18:37:02 -0300

    add signup POST request to auth.http

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  getHello(): string {
+    throw new Error('Method not implemented.');
+  }
+}
